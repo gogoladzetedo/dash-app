56cf79214c757ea67da8c93587c32895133a43f1
Tedo Gogoladze <gogoladzetedo@gmail.com>
2021-12-12 17:47:38 +0200

    added plotting

--- tutorial/python_pandas_basics/1_datareader.py
+++ tutorial/python_pandas_basics/1_datareader.py
@@ -92,7 +92,7 @@ with open('newfile.json', 'w') as fp:
 from pandas_datareader import data
 import math
 
-df = data.get_data_yahoo(['MSFT', 'AMZN', 'TSLA'], '2021-01-01', '2021-09-30')
+df = data.get_data_yahoo(['MSFT', 'AMZN', 'TSLA'], '2021-08-01', '2021-09-30')
 df = df.reset_index()
 df = df[['Close', 'Date']]
 df.columns.droplevel()
@@ -100,10 +100,14 @@ df.columns = df.columns.droplevel()
 df = df.rename(columns = {'':'Date'})
 
 
+
 df['new_col'] = 0
 df['new_col2'] = math.nan
 df['new_col3'] = df['MSFT']
 df['new_col3'] = df['MSFT'].apply(lambda x: x * 2)
+
+df[df['MSFT'] >= 300]
+df[( (df['MSFT'] >= 290) & df['MSFT'] <=310 )]
 
 for col in df.columns:
     print(col, df[col].max(), df[col].min(), df[col].mean())
@@ -116,4 +120,19 @@ print(df.loc[1], '\n', df.iloc[1])
 
 for i in range(0, len(df)):
     df['MSFT'].loc[i]
-    
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(df['Date'], df['MSFT'])
+plt.plot(df['Date'], df['MSFT'].apply(lambda x: x* 0))
+plt.plot(df['Date'], df['TSLA'], marker="o")
+#plt.show()
+
+import plotly.graph_objects as go
+fig = go.Figure()
+graph_data = df
+for ticker in ['AMZN', 'MSFT', 'TSLA']:
+    fig.add_trace(go.Scatter(x=graph_data['Date'], y=graph_data[ticker]
+    , name = ticker, mode='lines'))
+fig.show()
